3d006d169fadc028362eccb7da0d7bb2f97a01dc
lobe <lobergstad@gmail.com>
2025-10-02 16:40:13 +0200

    hej

--- sampling_1.py
+++ sampling_1.py
@@ -1,11 +1,17 @@
 import numpy as np
 from scipy import signal
+import control as ct
 
 f_s = 24000
 g_stop = 10 # Minsta dB reduktionen i stoppbandet, hittar på 10
 parameters = signal.cheb1ord(8000, 11000, 3, g_stop, analog = True) #Beräknar ordnignen som vi behöver på vårat chebichev filter
-print('Chebichev1-filter, ordning:', parameters[0])
+#print('Chebichev1-filter, ordning:', parameters[0])
 
 # Behöver ordning 3, ger tillbaka frekvens 8000
 
-#filter = signal.cheby1(3, 3)
+filter = signal.cheby1(3, 3, 8000, analog = True)
+#print(filter)
+
+H = ct.TransferFunction(filter)
+
+
