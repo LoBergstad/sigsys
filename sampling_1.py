075034986146a3eb6369d4c159c2c6b3519154a7
lobe <lobergstad@gmail.com>
2025-10-02 16:31:54 +0200

    cheby start

--- sampling_1.py
+++ sampling_1.py
@@ -2,7 +2,10 @@ import numpy as np
 from scipy import signal
 
 f_s = 24000
-g_stop = 10 # Minsta dB reduktionen i stoppbandet
+g_stop = 10 # Minsta dB reduktionen i stoppbandet, hittar på 10
 parameters = signal.cheb1ord(8000, 11000, 3, g_stop, analog = True)    
 print(parameters)
 
+# Behöver ordning 3, ger tillbaka frekvens 8000
+
+filter = signal.cheby1(3, 3)
